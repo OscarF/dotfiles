2e3b44e1d8380f961ef5529042b303bd9d0e27c6
Oscar Franzén <oscar.franzen@gmail.com>
2014-10-13 15:16:14 +0800

    Fix OSX PATH to have stuff in correct order

--- source/30_osx.sh
+++ source/30_osx.sh
@@ -1,2 +1,9 @@
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+# APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
+PATH=/usr/local/bin:$(path_remove /usr/local/bin)
+export PATH
+
 # Start ScreenSaver. This will lock the screen if locking is enabled.
 alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
