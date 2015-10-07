5f32b3694132bb6af3a8749b7094625b1b1aeb22
Oscar Franzén <oscar.franzen@klarna.com>
2015-10-07 22:41:50 +0200

    Added function, aliases and readline stuff

--- source/30_aliases.sh
+++ source/30_aliases.sh
@@ -9,5 +9,10 @@ alias ll='ls -l'
 alias la='ls -A'
 alias lal='ls -Al'
 alias l='ls -CF'
+# List only directories
+alias lsd='ls -l | grep "^d"'
 
 alias bar='echo -e "================================================================================"'
+
+
+alias cask='brew cask'
