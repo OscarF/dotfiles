654d2613bff90f413c77af99e4a990a3267804ae
Oscar Franzén <OscarF@users.noreply.github.com>
2018-10-22 18:43:40 +0200

    Update config

--- source/30_osx.sh
+++ source/30_osx.sh
@@ -4,6 +4,3 @@
 # APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
 PATH=/usr/local/bin:$(path_remove /usr/local/bin)
 export PATH
-
-# Start ScreenSaver. This will lock the screen if locking is enabled.
-alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
