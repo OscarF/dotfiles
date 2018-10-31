3b2bf1b06d75b7ef2f49ac0f6312fee6231e2579
Oscar Franzén <OscarF@users.noreply.github.com>
2018-10-31 19:10:10 +0100

    Updated bash autocompletion settings

--- source/10_misc.sh
+++ source/10_misc.sh
@@ -1,7 +1,8 @@
 # export MAKE_OPTS="-j 3"
 
-if type brew &> /dev/null && [ -f $(brew --prefix)/etc/bash_completion ]; then
-  source $(brew --prefix)/etc/bash_completion
+# Enable bash completion
+if type brew &> /dev/null && [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+  source $(brew --prefix)/share/bash-completion/bash_completion
 fi
 
 if type npm &> /dev/null && [ -f $(npm prefix -g)/lib/node_modules/npm/lib/utils/completion.sh ]; then
