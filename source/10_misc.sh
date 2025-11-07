84d53702321cd8eb9b5492cc45865de7a941bedb
Oscar <37442+OscarF@users.noreply.github.com>
2025-11-07 13:10:28 +0100

    Bash profile and completions

--- source/10_misc.sh
+++ source/10_misc.sh
@@ -1,14 +1,5 @@
 # export MAKE_OPTS="-j 3"
 
-# Enable bash completion
-if type brew &> /dev/null && [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
-  source $(brew --prefix)/share/bash-completion/bash_completion
-fi
-
-if type npm &> /dev/null && [ -f $(npm prefix -g)/lib/node_modules/npm/lib/utils/completion.sh ]; then
-  source $(npm prefix -g)/lib/node_modules/npm/lib/utils/completion.sh
-fi
-
 # Make Tab autocomplete regardless of filename case
 set completion-ignore-case on
 
--- source/20_homebrew_completions.sh
+++ source/20_homebrew_completions.sh
@@ -6,10 +6,9 @@ then
   if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
   then
     source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
-  else
-    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
-    do
-      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
-    done
   fi
+fi
+
+if type npm &> /dev/null && [ -f $(npm prefix -g)/lib/node_modules/npm/lib/utils/completion.sh ]; then
+  source $(npm prefix -g)/lib/node_modules/npm/lib/utils/completion.sh
 fi
